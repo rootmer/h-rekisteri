d95f043ec4858667cbd7fb3273bec4686325337f
rootmer <rootmer@gmail.com>
2014-09-05 10:04:28 +0300

    Korjattu kommentointia sekä ohjelman logiikka huomaa nyt jos taulukko täynnä. Eli ei voi lisätä täyteen tauluun enää tietoja.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 /** Ohjelma joka ottaa vastaan käyttäjältä
-*   henkilön perustietoja. Kommentoitia lisää
-*   myöhemmin..
+*   henkilön perustietoja. Kun taulukko täynnä
+*   ohjelma virheellisesti voi lisätä vielä tietoja
+*   jotka menevät viimeiseen soluun
 */
 
 #include <iostream>
@@ -16,15 +17,18 @@ struct tietue {
     string etunimi;
     float koulumatka;
     int hattukoko;
-    tietue(): etunimi("empty") {}
+    tietue(): etunimi("empty"), koulumatka(0.0), hattukoko(0) {}
 };
+
+const int taulunkoko = 2;
 
 int main()
 {
     // tietuetaulukon kokona 10 alkiota
-    tietue hRekisteri[10];
+    tietue hRekisteri[taulunkoko];
     int valinta = 99;
     int kohta = 0;
+    bool taynna = false;
 
     do {
         // Valikko näkyville
@@ -37,29 +41,42 @@ int main()
 
         if (valinta == 0) { break; }
 
+        /** Oletuksena taulukon etunimi- jäsen on "empty" jotta voidaan tietää mikä
+        *   alkio on ns. tyhjä. Ohjelma siis etsii aluksi tyhjän kohdan johon voi lisätä
+        *   henkilötietoja.
+        */
         else if (valinta == 1) {
-                for (int a = 0; a < 10; a++) {
+                for (int a = 0; a < taulunkoko; a++) {
                     if (hRekisteri[a].etunimi == "empty") {
                         kohta = a;
+                        break;
                     }
                 }
 
-                string eNimi;
-                float kMatka;
-                int hKoko;
-                cout << "Henkilön etunimi" << endl;
-                cin >> eNimi;
-                cout << "Henkilön koulumatka" << endl;
-                cin >> kMatka;
-                cout << "Henkilön hatun koko" << endl;
-                cin >> hKoko;
-                hRekisteri[kohta].etunimi = eNimi;
-                hRekisteri[kohta].koulumatka = kMatka;
-                hRekisteri[kohta].hattukoko = hKoko;
+                if (!taynna) {
+                    string eNimi;
+                    float kMatka;
+                    int hKoko;
+                    cout << "Henkilön etunimi" << endl;
+                    cin >> eNimi;
+                    cout << "Henkilön koulumatka" << endl;
+                    cin >> kMatka;
+                    cout << "Henkilön hatun koko" << endl;
+                    cin >> hKoko;
+                    hRekisteri[kohta].etunimi = eNimi;
+                    hRekisteri[kohta].koulumatka = kMatka;
+                    hRekisteri[kohta].hattukoko = hKoko;
+                    if (kohta == taulunkoko - 1) { taynna = true; }
+                }
+
+                else if (taynna) { cout << endl << "Taulukko täynnä!" << endl; }
         }
 
+        /** Listaa henkilötiedot
+        *   taulukosta
+        */
         else if (valinta == 2) {
-            for (int a = 0; a < 10; a++) {
+            for (int a = 0; a < taulunkoko; a++) {
                 cout << "Henkilö " << a << ": " << hRekisteri[a].etunimi << " "
                 << hRekisteri[a].koulumatka << " " << hRekisteri[a].hattukoko << endl;
             }
